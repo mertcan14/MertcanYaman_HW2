0b9058e8d8fc269b1cb4e249956147611c3bdd93
mertcanyaman <mert1437@gmail.com>
2023-05-11 15:28:41 +0300

    create project

---
+++ MertcanYaman_HW2/MertcanYaman_HW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MertcanYaman_HW2
+//
+//  Created by mertcan YAMAN on 11.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
